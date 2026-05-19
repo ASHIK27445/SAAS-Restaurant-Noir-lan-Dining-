f92ee0597d89009faf2e30846f0efc06f8620e80
ASHIK27445 <mdashikulislama57@gmail.com>
2026-05-20 03:38:24 +0600

    feat: Add React Query Devtools for enhanced debugging and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,19 @@ import { RouterProvider } from 'react-router'
 import { router } from './Routes/Routes.tsx'
 import AuthProvider from './components/Authentication/AuthProvider.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <RouterProvider router={router}>
-      </RouterProvider>
-    </AuthProvider></QueryClientProvider>
+      <AuthProvider>
+        <RouterProvider router={router}>
+        </RouterProvider>
+      </AuthProvider>
+
+       <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 )
